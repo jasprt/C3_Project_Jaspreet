c670c89345db755a9889031c9c5779bb9e61b643
jaspreet <singhjaspreet821@gmail.com>
2021-01-11 01:37:29 +0530

    Part 2: Solution

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,76 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Restaurant {
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private String name;
+    private String location;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        LocalTime timeNow = getCurrentTime();
+        return timeNow.isAfter(openingTime) && timeNow.isBefore(closingTime);
+
+    }
+
+    public LocalTime getCurrentTime() {
+        return LocalTime.now();
+    }
+
+    public List<Item> getMenu() {
+        if (isRestaurantOpen()) return this.menu;
+        else {
+            System.out.println("Restaurant is closed! Visit between: " + openingTime + " & " + closingTime);
+            return Collections.emptyList();
+        }
+    }
+
+    private Item findItemByName(String itemName) {
+        for (Item item : menu) {
+            if (item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name, price);
+        menu.add(newItem);
+    }
+
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+
+    public void displayDetails() {
+        System.out.println("Restaurant:" + name + "\n"
+                + "Location:" + location + "\n"
+                + "Opening time:" + openingTime + "\n"
+                + "Closing time:" + closingTime + "\n"
+                + "Menu:" + "\n" + getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
